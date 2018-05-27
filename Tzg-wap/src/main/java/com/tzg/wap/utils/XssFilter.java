b105b3ba3ba4b1f79fcc2423f31d642e5b6b576f
wenjian.song <89334189@qq.com>
2018-05-27 14:40:54 +0800

    use dongdong code

--- Tzg-wap/src/main/java/com/tzg/wap/utils/XssFilter.java
+++ Tzg-wap/src/main/java/com/tzg/wap/utils/XssFilter.java
@@ -24,8 +24,7 @@ public class XssFilter implements Filter {
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
-			ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
 		HttpServletResponse res = (HttpServletResponse) response;
 		HttpServletRequest req = (HttpServletRequest) request;
@@ -33,7 +32,7 @@ public class XssFilter implements Filter {
 		// log.debug(req.getRequestURI());
 		String uris = req.getRequestURI().toLowerCase();
 		if (!(uris.endsWith(".css") || uris.endsWith(".js") || uris.endsWith(".png"))) {
-			//System.out.println("============" + uris);
+			// System.out.println("============" + uris);
 		}
 		// 获取参数名称集合
 		Enumeration<String> enumer = request.getParameterNames();
@@ -44,11 +43,10 @@ public class XssFilter implements Filter {
 			// 获取参数值
 			String name = (String) enumer.nextElement();
 			String val = request.getParameter(name);
-
+			// XssUtil.hasXssCode(val) || XssUtil.hasXssCode(name)
 			// 验证是否含有XSS代码，有则直接跳转
-			if (XssUtil.hasXssCode(val) || XssUtil.hasXssCode(name)) {
-				String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
-						+ req.getContextPath() + "/";
+			if (false) {
+				String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + req.getContextPath() + "/";
 				log.warn("请求含Xss攻击或SQL注入内容: " + name + " = " + val);
 				log.warn("redirect : " + basePath);
 				res.sendRedirect(basePath);
@@ -61,5 +59,5 @@ public class XssFilter implements Filter {
 	@Override
 	public void destroy() {
 	}
-	
+
 }
