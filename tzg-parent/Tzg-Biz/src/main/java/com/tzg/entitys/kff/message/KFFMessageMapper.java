406924b67eb287d2a339c0aa20c5609b35355b1e
wenjian.song <89334189@qq.com>
2018-05-27 09:31:17 +0800

    remove tzg-parent folder

--- tzg-parent/Tzg-Biz/src/main/java/com/tzg/entitys/kff/message/KFFMessageMapper.java
+++
@@ -1,9 +0,0 @@
-package com.tzg.entitys.kff.message;
-
-import com.tzg.common.base.BaseMapper;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KFFMessageMapper extends BaseMapper<KFFMessage, java.lang.Integer> {	
-
-}
