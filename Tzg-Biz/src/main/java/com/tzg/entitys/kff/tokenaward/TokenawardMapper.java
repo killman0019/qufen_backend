f0fba3134d1cddc8138154b98f5a4b32eecc3e31
wanghan <wanghan@qufen.io>
2018-05-28 10:42:23 +0800

    新增tokenaward实体

---
+++ Tzg-Biz/src/main/java/com/tzg/entitys/kff/tokenaward/TokenawardMapper.java
@@ -0,0 +1,28 @@
+package com.tzg.entitys.kff.tokenaward;
+
+import com.tzg.common.base.BaseMapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenawardMapper extends BaseMapper<Tokenaward, java.lang.Integer> {
+
+	void save(Tokenaward tokenaward);
+
+	void update(Tokenaward tokenaward);
+	List<Tokenaward> findByUserId(Integer id);
+	Double reawardSum(Integer userId,Integer functionType);
+	List<Tokenaward> priaiseAwardSum(Integer userId);
+
+	Double findUserSumRewardToken(Integer userId, Integer functionType);
+
+	Double findUserSumInviteRewards(Integer userId, Integer functionType);
+	<E> Double findUserSumInviteRewards(Map<E, E> map);
+
+	List<Tokenaward> findAllTokenawardByWhere(Map<String, Object> map);
+
+	void updateByGrantType(Integer a);
+}
