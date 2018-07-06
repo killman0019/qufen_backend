abeabb4de8ac75d930e18d7ed654c0fc714876d2
linj <664678659@qq.com>
2018-07-06 17:01:50 +0800

    无修改

--- Tzg-Biz/src/main/java/com/tzg/rmi/service/KFFRmiService.java
+++ Tzg-Biz/src/main/java/com/tzg/rmi/service/KFFRmiService.java
@@ -57,6 +57,7 @@ import com.tzg.entitys.loginaccount.RegisterRequest;
 import com.tzg.entitys.photo.PhotoParams;
 import com.tzg.rest.exception.rest.RestServiceException;
 
+
 /**
  * kff 相关远程服务调用
  * 
@@ -867,4 +868,11 @@ public interface KFFRmiService {
 	 * @throws RestServiceException
 	 */
 	public Integer selectUserStatusByPhone(String phone) throws RestServiceException;
+
+	/**
+	 * 将用户的pop状态设置成0
+	 * @param loginUserId
+	 * @throws RestServiceException
+	 */
+	public void updateUserKFFsetPopZero(Integer loginUserId) throws RestServiceException;
 }
