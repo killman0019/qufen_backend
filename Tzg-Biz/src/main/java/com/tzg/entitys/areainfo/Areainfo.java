470667027c96368f0399b3d8fea05ebf032c6716
linj <664678659@qq.com>
2018-08-10 18:23:05 +0800

    测试jenkins

--- Tzg-Biz/src/main/java/com/tzg/entitys/areainfo/Areainfo.java
+++ Tzg-Biz/src/main/java/com/tzg/entitys/areainfo/Areainfo.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 
 public class Areainfo implements Serializable {
 	private static final long serialVersionUID = 1L;
-    /**
-     * id
-     */ 	
+	
 	private java.lang.Integer id;
     /**
      * vcCode
