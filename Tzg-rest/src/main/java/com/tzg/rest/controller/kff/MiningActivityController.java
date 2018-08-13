549630e86c418824ac762a6a8c80b05cea23c153
linj <664678659@qq.com>
2018-08-13 11:37:25 +0800

    修改挖矿排序

--- Tzg-rest/src/main/java/com/tzg/rest/controller/kff/MiningActivityController.java
+++ Tzg-rest/src/main/java/com/tzg/rest/controller/kff/MiningActivityController.java
@@ -310,7 +310,7 @@ public class MiningActivityController extends BaseController {
 			if(state==0) {
 				query.addQueryData("status1", 0);
 				query.addQueryData("status2", 1);
-				query.addQueryData("status3", 4);
+//				query.addQueryData("status3", 4);
 			}
 			if(state==1) {
 				query.addQueryData("status4", 2);
@@ -322,6 +322,13 @@ public class MiningActivityController extends BaseController {
 	        query.setPageIndex(curPage);
 	        query.setRowsPerPage(pageSize);
 			PageResult<MiningActivity> data = miningActivityRmiService.findMiningActivityPage(query);
+			if(data==null) {
+				PaginationQuery queryc = new PaginationQuery();
+				queryc.addQueryData("status3", 4);
+				queryc.setPageIndex(curPage);
+				queryc.setRowsPerPage(pageSize);
+				data = miningActivityRmiService.findMiningActivityPage(query);
+			}
             map.put("data", data);
 			bre.setData(map);
 		} catch (RestServiceException e) {
