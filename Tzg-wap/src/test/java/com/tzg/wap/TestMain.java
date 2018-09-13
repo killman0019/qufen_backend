e29a26d6cea9b541afde5b18f8f5f0b2c3e33da2
zhangdongdong <zhangdongdong@qufen.io>
2018-09-13 16:43:32 +0800

    去除问题

--- Tzg-wap/src/test/java/com/tzg/wap/TestMain.java
+++ Tzg-wap/src/test/java/com/tzg/wap/TestMain.java
@@ -5,16 +5,16 @@ import java.util.TimerTask;
 
 public class TestMain {
 
-	public static void main(String[] args) {
-		Timer timer = new Timer();// 实例化Timer类
-		timer.schedule(new TimerTask() {
-			public void run() {
-				System.out.println("退出");
-				timer.cancel();
-
-			}
-		}, 5000);// 这里百毫秒
-		System.out.println("本程序存在5秒后自动退出");
-
-	}
+	// public static void main(String[] args) {
+	// Timer timer = new Timer();// 实例化Timer类
+	// timer.schedule(new TimerTask() {
+	// public void run() {
+	// System.out.println("退出");
+	// timer.cancel();
+	//
+	// }
+	// }, 5000);// 这里百毫秒
+	// System.out.println("本程序存在5秒后自动退出");
+	//
+	// }
 }
