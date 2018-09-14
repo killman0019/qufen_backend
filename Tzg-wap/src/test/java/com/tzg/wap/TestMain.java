1044f51fe6a16d35b2a29499937b78e8793afbd3
zhangdongdong <zhangdongdong@qufen.io>
2018-09-14 14:11:14 +0800

    合并代码

--- Tzg-wap/src/test/java/com/tzg/wap/TestMain.java
+++ Tzg-wap/src/test/java/com/tzg/wap/TestMain.java
@@ -3,18 +3,12 @@ package com.tzg.wap;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class TestMain {
+import com.tzg.common.utils.DateUtil;
 
-	// public static void main(String[] args) {
-	// Timer timer = new Timer();// 实例化Timer类
-	// timer.schedule(new TimerTask() {
-	// public void run() {
-	// System.out.println("退出");
-	// timer.cancel();
-	//
-	// }
-	// }, 5000);// 这里百毫秒
-	// System.out.println("本程序存在5秒后自动退出");
-	//
-	// }
+public class TestMain {
+	public static void main(String[] args) {
+
+		String postCreateBegin = DateUtil.getSpecifiedDayBeforeOrAfter(1);
+		System.err.println(postCreateBegin);
+	}
 }
